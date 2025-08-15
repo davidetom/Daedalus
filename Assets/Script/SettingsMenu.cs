937210181d726b302f2df778110fd244d0e87de1
Fedelele <llricowu01970@gmail.com>
2025-08-15 18:29:05 +0200

    feat(menu): add MainMenu scene, UI assets, settings menu script

---
+++ Assets/Script/SettingsMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
